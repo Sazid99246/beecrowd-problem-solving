8b7a7056607f10892445311a0cc75b0f01987bf9
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-14 23:05:24 +0600

    BEE1098 problem solved

---
+++ src/BEE1098.java
@@ -0,0 +1,14 @@
+public class BEE1098 {
+    public static void main(String[] args) {
+        for (float i = 0; i <= 2.01; i += 0.2){
+            for (int j = 1; j < 4; j ++){
+                if (i == 0 || i == 1 || i >= 2){
+                    System.out.printf("I=%.0f J=%.0f\n", i, j+i);
+                }
+                else{
+                    System.out.printf("I=%.1f J=%.1f\n", i, j+i);
+                }
+            }
+        }
+    }
+}
