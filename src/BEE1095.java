02029a15ed16da5b5e84a05b83a07029a1a33133
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-14 19:56:17 +0600

    BEE1095 problem solved

---
+++ src/BEE1095.java
@@ -0,0 +1,8 @@
+public class BEE1095 {
+    public static void main(String[] args) {
+        int j = 60;
+        for (int i = 1; j >= 0; i += 3, j -= 5) {
+            System.out.println("I=" + i + " J=" + j);
+        }
+    }
+}
