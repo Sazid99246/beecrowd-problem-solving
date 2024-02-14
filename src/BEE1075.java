fd8c9e8cb91b17d09859ba3252757cba39383fbc
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-14 18:41:52 +0600

    BEE1075 problem solved

---
+++ src/BEE1075.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BEE1075 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= 10000; i++) {
+            if (i % n == 2) {
+                System.out.println(i);
+            }
+        }
+
+        scanner.close();
+    }
+}
