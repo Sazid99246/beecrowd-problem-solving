fa74a78440bafb2567e983d4802951de5ea9db44
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-15 17:10:59 +0600

    BEE1149 problem solved

---
+++ src/BEE1149.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class BEE1149 {
+	
+    public static void main(String[] args) {
+        Scanner leitor = new Scanner(System.in);
+        int A = leitor.nextInt();
+        int N = leitor.nextInt();
+        int soma = 0;
+        while (N <= 0) {
+        	N = leitor.nextInt();
+        }
+        for (int i = 1; i <= N; i++) {
+        	soma += A;
+        	A++;
+        }
+        System.out.println(soma);
+        leitor.close();
+    }
+	
+}
