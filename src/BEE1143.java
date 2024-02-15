094770974c8b5d90c51b6009fd7ad0edb3f70326
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-15 11:28:11 +0600

    BEE1143 problem solved

---
+++ src/BEE1143.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class BEE1143 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " " + (i * i) + " " + (i * i * i));
+        }
+
+        scanner.close();
+    }
+}
