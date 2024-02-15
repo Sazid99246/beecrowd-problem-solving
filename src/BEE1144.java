3df6f3eb9db80e49068f9433ac5bedd599ccbf6c
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-15 11:30:31 +0600

    BEE1144 problem solved

---
+++ src/BEE1144.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class BEE1144 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println(i + " " + (i * i) + " " + (i * i * i));
+            System.out.println(i + " " + (i * i + 1) + " " + (i * i * i + 1));
+        }
+
+        scanner.close();
+    }
+}
