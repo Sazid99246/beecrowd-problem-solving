b21427744035f5217c266e83daf502b2a4840c1e
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-02-17 16:24:15 +0600

    BEE1153 problem solved

---
+++ src/BEE1153.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class BEE1152 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int n = scanner.nextInt();
+
+        System.out.print(factorial(n));
+
+        scanner.close();
+    }
+
+    private static int factorial(int n) {
+        int prod;
+        if (n == 0) {
+            prod = 0;
+        } else if (n == 1) {
+            prod = 1;
+        } else {
+            prod = n * factorial(n - 1);
+        }
+        return prod;
+    }
+}
