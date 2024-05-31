e09fa65c715e80ce488f67ea583872a0788af649
Sazid99246 <sheikhmdsazidulislam@gmail.com>
2024-05-31 21:49:04 +0600

    BEE 2065 problem solved

--- src/BEE2065.sql
+++ src/BEE2065.sql
@@ -1,4 +1,5 @@
-SELECT p.name, f.name
-FROM products p INNER JOIN providers f ON
-     p.id_providers	= f.id
+SELECT p.name,
+    f.name
+FROM products p
+    INNER JOIN providers f ON p.id_providers = f.id
 WHERE p.id_categories = 6
